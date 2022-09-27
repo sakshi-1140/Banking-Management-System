540c8050a7cf4d1b7488a2b5206bc741e9d19ed2
Sakshi <sakshichoudhary000@gmail.com>
2022-09-27 11:06:35 +0530

    Banking Management System

---
+++ src/dbconnect.java
@@ -0,0 +1,30 @@
+/*
+ * Document: MyPage.java
+ * National Institute Of Technology, Uttarakhand
+ * @authors Sakshi Choudhary
+ * Document: contains database connect
+ */
+
+/**
+ *
+ * @authors Sakshi Choudhary
+ */
+import java.sql.*;
+import javax.swing.JOptionPane;
+public class dbconnect {
+    Connection conn = null;
+/* In this section please change the path to connect to the database.
+*/
+    public static Connection connectDb(){
+        try{
+            Class.forName("org.sqlite.JDBC");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:bank.sqlite");
+            
+            return conn;
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null,e);
+        }
+        return null;
+    }
+    
+}
